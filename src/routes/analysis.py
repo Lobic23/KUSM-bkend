d524105577be980c5081c4884c7b3ec9c48110d3
samki <ssameep5@gmail.com>
2026-01-06 16:44:24 +0000

    endpoint to get monthly average

--- src/routes/analysis.py
+++ src/routes/analysis.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy import func, desc
 from sqlalchemy.orm import Session
-from ..models import EnergyDB, MeterDB, PowerDB, VoltageDB
+from ..models import EnergyDB, MeterDB, PowerDB, CurrentDB, VoltageDB, VoltageDB
 from ..database import get_db
+from ..api.iammeter import get_meter_id_by_name
+from datetime import datetime
 from ..api.iammeter import voltage_unbalance_status, calculate_voltage_unbalance
 
 router = APIRouter(prefix="/analysis", tags=["analysis"])
@@ -29,7 +31,84 @@ def get_consumption_and_power(db: Session = Depends(get_db)):
         "success": True,
         "result": result
     }
+    
+MONTHS = {
+    1: "jan", 2: "feb", 3: "mar", 4: "apr",
+    5: "may", 6: "jun", 7: "jul", 8: "aug",
+    9: "sep", 10: "oct", 11: "nov", 12: "dec"
+}
+@router.get("/monthly_average/{year}/{meter_name}")
+def monthly_average(meter_name: str, year: int, db:Session = Depends(get_db)):
+    data = {}
+    meter_id = get_meter_id_by_name(meter_name)
+    for month in range(1, 13):
+        start_date = datetime(year, month, 1)
+        end_date = (
+            datetime(year + 1, 1, 1)
+            if month == 12
+            else datetime(year, month + 1, 1)
+        )
 
+        avg_current = db.query(
+            (
+                func.coalesce(func.avg(CurrentDB.phase_A_current), 0) +
+                func.coalesce(func.avg(CurrentDB.phase_B_current), 0) +
+                func.coalesce(func.avg(CurrentDB.phase_C_current), 0)
+            )
+        ).filter(
+            CurrentDB.meter_id == meter_id,
+            CurrentDB.timestamp >= start_date,
+            CurrentDB.timestamp < end_date
+        ).scalar()
+        
+        avg_voltage = db.query(
+            (
+                func.coalesce(func.avg(VoltageDB.phase_A_voltage), 0) +
+                func.coalesce(func.avg(VoltageDB.phase_B_voltage), 0) +
+                func.coalesce(func.avg(VoltageDB.phase_C_voltage), 0)
+            )
+        ).filter(
+            VoltageDB.meter_id == meter_id,
+            VoltageDB.timestamp >= start_date,
+            VoltageDB.timestamp < end_date
+        ).scalar()       
+
+        avg_power = db.query(
+            (
+                func.coalesce(func.avg(PowerDB.phase_A_active_power), 0) +
+                func.coalesce(func.avg(PowerDB.phase_B_active_power), 0) +
+                func.coalesce(func.avg(PowerDB.phase_C_active_power), 0)
+            )
+        ).filter(
+            PowerDB.meter_id == meter_id,
+            PowerDB.timestamp >= start_date,
+            PowerDB.timestamp < end_date
+        ).scalar()
+
+        avg_energy = db.query(
+            (
+                func.coalesce(func.avg(EnergyDB.phase_A_grid_consumption), 0) +
+                func.coalesce(func.avg(EnergyDB.phase_B_grid_consumption), 0) +
+                func.coalesce(func.avg(EnergyDB.phase_C_grid_consumption), 0)
+            )
+        ).filter(
+            EnergyDB.meter_id == meter_id,
+            EnergyDB.timestamp >= start_date,
+            EnergyDB.timestamp < end_date
+        ).scalar()
+
+        data[MONTHS[month]] = {
+            "average_current": avg_current,
+            "average_voltage":avg_voltage,
+            "average_power": avg_power,
+            "average_energy": avg_energy
+        }
+
+    return {
+        "success": True,
+        "year": year,
+        "data": data
+    }
 @router.get("/voltage")
 def get_voltage_unbalance(db: Session = Depends(get_db)):
     meters = db.query(MeterDB).all()
